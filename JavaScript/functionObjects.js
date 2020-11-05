5e1d43ee70f378ac55951e659c7edd2a11517007
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2020-11-05 18:39:44 +0530

    changes done

--- JavaScript/functionObjects.js
+++ JavaScript/functionObjects.js
@@ -1,11 +1,11 @@
 function abc(){
     abc.x= 10;
     abc.y= 20;
-    abc.hello=function()
+    abc.hello = function()
     {
-        console.log("hello i'm a function within a function");
+        console.log("hello i am a function within a function");
     };
-}
+};
 console.log(abc.x);
 console.log(abc.y);
-//console.log(abc.hello());
+console.log(abc.hello());
