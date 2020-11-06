1d4854b309df554bf7c8825fac618ec7acfadb84
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2020-11-06 15:02:20 +0530

    live-clock

--- JavaScript/Live-Clock/main.js
+++ JavaScript/Live-Clock/main.js
@@ -5,6 +5,10 @@ const name= document.getElementById("name");
 const date= document.getElementById("date");        //shift+alt+down arrow to copy code
 
 //Event Listener
+
+name.addEventListener("keypress",setName);
+name.addEventListener("blur",setName);
+
 //Function
 function showtime()
 {
@@ -32,5 +36,61 @@ function showtime()
         return ((parseInt(n,10)<10?'0':'')+n)       //05
     }
 }
+
+function setGreeting()
+{
+    let today = new Date();              //Date(2020,12,12,20,12,11)
+    let hour = today.getHours();
+
+    if(hour<12)
+    {
+        document.body.style.backgroundImage= 'url("img/morning1.jpg")';
+        
+        greeting.innerHTML ='Good Morning :)';
+    }
+    else if(hour<18)
+    {
+        document.body.style.backgroundImage= 'url("img/evening.jpg")';
+        greeting.innerHTML ='Good Evening :)';
+        document.body.style.color='white';
+    }
+    else{
+        document.body.style.backgroundImage= 'url("img/night.jpg")';
+        greeting.innerHTML ='Good Night :)';
+        document.body.style.color='white';
+    }
+}
+
+function getName()
+{
+    if(localStorage.getItem('myName')===null)
+    {
+        name.innerHTML= "[Enter Name]";
+    }
+    else{
+        name.innerHTML= localStorage.getItem('myName');
+    }
+}
+
+//create a function to store data in local storage
+
+function setName(e)
+{
+    if(e.type==="keypress")
+    {
+        if(e.keyCode==13)
+        {
+            localStorage.setItem('myName',e.target.innerHTML);
+            name.blur();
+        }
+    }
+    else
+    {
+        localStorage.setItem('myName',e.target.innerHTML);
+    }
+}
+
 //Function Call
 showtime();
+setGreeting();
+getName();
