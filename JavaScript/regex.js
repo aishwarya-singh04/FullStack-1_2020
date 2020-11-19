76c212f54680ed11b76847ffcf9ab6109d06edb0
aishwarya-singh04 <aishwarya.singh_cs18@gla.ac.in>
2020-11-19 15:51:26 +0530

    regex

---
+++ JavaScript/regex.js
@@ -0,0 +1,8 @@
+//Method 1 : Using RegExp Constructor
+//var pattern = new RegExp("pattern", attribute);
+
+//modifiers are optional attributes g|i|m
+
+//method-2: shorthand notation
+
+// var pattrn =/abc/g;
